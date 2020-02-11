cc1e89ada427acaff0392029e632ff7ae83fee02
darodriguez2 <darodriguez2@outlook.com>
2020-02-11 16:23:42 -0500

    Update ConnectToAPI2.java

--- connectToAPI2/src/connecttoapi2/ConnectToAPI2.java
+++ connectToAPI2/src/connecttoapi2/ConnectToAPI2.java
@@ -9,49 +9,58 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.*;
 /**
- *
- * @author diego
+ * @author Diego
  */
 public class ConnectToAPI2 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
      public static void main(String[] args) {
-        String baseUrl = "https://developers.zomato.com/api";
+         
+        //parameters needed for urlString 
+        String requestURL = "https://developers.zomato.com/api";
         String version = "/v2.1";
         String getRequest = "/geocode";
         final String lat = "lat=36.066984";
         final String lon = "lon=-79.800178";
-        String apiKey = "680201a1d38ff559b9c8b20ffde6db61";
-        String urlString = baseUrl + version + getRequest + "?" + lat + "&" + lon;
+        String apiKey = "0a71dc953812d0958a14168a49b5acfd";
+        String urlString = requestURL + version + getRequest + "?" + lat + "&" + lon;
         
+        //create url Object
         URL url;
-        System.out.println("Marker 1");
+        
         try {
-        System.out.println("Marker 2");
+            
             url = new URL(urlString);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            
+            //configure HttpURLConnection object
             con.setRequestProperty("Accept", "application/json" );
-            con.setRequestProperty("user-key", "0a71dc953812d0958a14168a49b5acfd" );
+            con.setRequestProperty("user-key", apiKey );
             con.setRequestMethod("GET");
-        System.out.println("Marker 3");
 
             int status = con.getResponseCode();
-            
             System.out.println("Response Code: " + status);
+            
+            //wrap InputStream in BufferedReader
+     `      //read data from the input stream and store it in a string. 
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
             StringBuffer content = new StringBuffer();
             while ((inputLine = in.readLine()) != null) {
                 content.append(inputLine);
             }
+            
+            //close connection
             in.close();
             con.disconnect();
+            
             System.out.println("Output: " + content.toString());
+            
+            //parse data from stringBuffer object using JSONObject
             JSONObject obj = new JSONObject(content.toString());
             String movieName = obj.getString("popularity");
             System.out.println("Top Cuisines: " + movieName);
+            
         } catch (Exception ex) {
             Logger.getLogger(ConnectToAPI2.class.getName()).log(Level.SEVERE, null, ex);
             return;
