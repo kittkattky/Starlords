f4acd0e375b886baf7223916550350be397014b0
Ky <47390988+kittkattky@users.noreply.github.com>
2020-02-13 17:54:30 -0500

    Delete fileOne.java

--- fileOne.java
+++
@@ -1,7 +0,0 @@
-/* We have abided by the UNCG Academic Integrity Policy. Kylia Hurley Preston, Diego Kahlie. 
-2/4/2020 
-This desktop application is named HeyFriday!. It allows user to choose an activity in their area 
-on specific day and time to go and do. For example users will have the option to save a profile and choose to
-go on a date at the movies with dinner. They will put in their zip code choose dinner and movie and then 
-be given the movie showings w/ theaters and resturants to go to. Possible options to buy tickets and reserve
-table to be added.*/
