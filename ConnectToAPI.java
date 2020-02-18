69a45c2b4f14f80ab843d4eec83cefb5d6c7ca5f
darodriguez2 <darodriguez2@outlook.com>
2020-02-18 16:36:41 -0500

    Add files via upload
    Prototype

--- ConnectToAPI.java
+++ ConnectToAPI.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package connecttoapi;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -13,51 +13,61 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.*;
 /**
- *
- * @author darod
+ * @author Diego
+ *gets popular cuisines and resturaunts around the specified coordinates
  */
 public class ConnectToAPI {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        String baseUrl = "https://api.themoviedb.org";
-        String callAction = "/3/movie/";
-        String movieId = "299536";
-        String apiKey = "680201a1d38ff559b9c8b20ffde6db61";
-        String urlString = "https://developers.zomato.com/api/v2.1/geocode?lat=36.066984&lon=-79.800178";
-        
+   
+     public static void main(String[] args) {
+         
+        //parameters needed for urlString
+        String requestURL = "https://developers.zomato.com/api";
+        String version = "/v2.1";
+        String getRequest = "/geocode";
+        final String lat = "lat=36.066984";
+        final String lon = "lon=-79.800178";
+        String apiKey = "0a71dc953812d0958a14168a49b5acfd";
+        String urlString = requestURL + version + getRequest + "?" + lat + "&" + lon;
+       
+        //create url Object
         URL url;
-        System.out.println("Marker 1");
+       
         try {
-        System.out.println("Marker 2");
+           
             url = new URL(urlString);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
+           
+            //configure HttpURLConnection object
             con.setRequestProperty("Accept", "application/json" );
-            con.setRequestProperty("user-key", "0a71dc953812d0958a14168a49b5acfd" );
+            con.setRequestProperty("user-key", apiKey );
             con.setRequestMethod("GET");
-        System.out.println("Marker 3");
-
             int status = con.getResponseCode();
-            
             System.out.println("Response Code: " + status);
+           
+            //wrap InputStream in BufferedReader
+            //read data from the input stream and store it in a string.
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
             StringBuffer content = new StringBuffer();
             while ((inputLine = in.readLine()) != null) {
                 content.append(inputLine);
             }
+           
+            //close connection
             in.close();
             con.disconnect();
+           
             System.out.println("Output: " + content.toString());
+           
+            //parse data from stringBuffer object using JSONObject
             JSONObject obj = new JSONObject(content.toString());
-            String movieName = obj.getString("popularity");
-            System.out.println("Top Cuisines: " + movieName);
+            String popularity = obj.getString("popularity");
+            System.out.println("Top Cuisines: " + popularity);
+           
         } catch (Exception ex) {
             Logger.getLogger(ConnectToAPI.class.getName()).log(Level.SEVERE, null, ex);
             return;
         }
     }
-    
+   
 }
