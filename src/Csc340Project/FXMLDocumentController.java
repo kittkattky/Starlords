f9ea040bb8fdc0b07b23099eac828552ebf2b028
Kahlie1 <39012827+Kahlie1@users.noreply.github.com>
2020-02-25 17:33:22 -0500

    Add files via upload

---
+++ src/Csc340Project/FXMLDocumentController.java
@@ -0,0 +1,143 @@
+package Csc340Project;
+
+/**
+ * This class is
+ *
+ * @author Kahlie Last Updated: 2/17/2020
+ */
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import utils.ConnectionUtil;
+
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Label labelError;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Button signIn;
+    
+    @FXML
+    private Button signUp;
+    
+    //Connecting to database
+    Connection Con = null;
+    //Used to execute SQL queries 
+    PreparedStatement PreparedStatement = null;
+    //Contains results of the SQL query
+    ResultSet ResultSet = null;
+
+    @FXML
+    public void handleButtonAction(MouseEvent event) {
+
+        if (event.getSource() == signIn) {
+            if (logIn().equals("Success")) {
+                try {
+
+                    Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+                    stage.close();
+                    Scene scene = new Scene(FXMLLoader.load(getClass().getClassLoader().getResource("fxml/HomePage.fxml")));
+                    stage.setScene(scene);
+                    stage.show();
+
+                } catch (IOException ex) {
+                    System.err.println(ex.getMessage());
+                }
+
+            }
+        }
+    }
+    
+    @FXML
+    public void handleButtonAction2(MouseEvent event) throws IOException{
+
+        if (event.getSource() == signUp) {
+                
+                    Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+                    stage.close();
+                    Scene scene = new Scene(FXMLLoader.load(getClass().getClassLoader().getResource("fxml/SignUpPage.fxml")));
+                    stage.setScene(scene);
+                    stage.show();
+        }
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        if (Con == null) {
+            labelError.setTextFill(Color.RED);
+            labelError.setText("Server Error");
+        } else {
+            labelError.setTextFill(Color.GREEN);
+            labelError.setText("Server Online");
+        }
+    }
+
+    public FXMLDocumentController() throws ClassNotFoundException {
+       Con = ConnectionUtil.conDB();
+        
+        //ConnectionUtil conn = new ConnectionUtil(); 
+    }
+    
+    //status check
+    private String logIn() {
+        String status = "Success";
+        String email1 = email.getText();
+        String password1 = password.getText();
+        if(email1.isEmpty() || password1.isEmpty()) {
+            setLabelError(Color.TOMATO, "Please Enter a Username and Password");
+            status = "Error";
+        } else {
+            //query
+            String sql = "SELECT * FROM USERS Where USERNAME = ? and PASSWORD = ?";
+            try {
+                PreparedStatement = Con.prepareStatement(sql);
+                PreparedStatement.setString(1, email1);
+                PreparedStatement.setString(2, password1);
+                ResultSet = PreparedStatement.executeQuery();
+                if (!ResultSet.next()) {
+                    setLabelError(Color.TOMATO, "Wrong Username or Password");
+                    status = "Error";
+                } else {
+                    setLabelError(Color.GREEN, "Login Successful");
+                }
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+                status = "Exception";
+            }
+        }
+        
+        return status;
+    }
+    
+    private void setLabelError(Color color, String text) {
+        labelError.setTextFill(color);
+        labelError.setText(text);
+        System.out.println(text);
+    }
+}
---
+++ src/Csc340Project/HomePageController.java
@@ -0,0 +1,23 @@
+package Csc340Project;
+
+/**
+ * This class is the home page
+ *
+ * @author Kahlie Last Updated: 2/21/2020
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class HomePageController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
