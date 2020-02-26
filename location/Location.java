9659dbde1f6aaaa7008ed9e360b22f024b2b7254
preston.williamson <p_willi7@uncg.edu>
2020-02-26 11:28:58 -0500

    Modifying APIController to reuse APIController constructor and pass null userKey.

--- location/Location.java
+++ location/Location.java
@@ -1,29 +1,26 @@
 package location;
 
-import Utilities.*;
 import API.*;
-import java.io.*;
-import java.net.MalformedURLException;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import java.util.LinkedHashMap;
 import org.json.JSONException;
-import org.apache.poi.xssf.usermodel.*;
 
 public class Location {
-     public static void main(String[] args) throws JSONException {        
+     public static void main(String[] args) throws JSONException {
+        final String userKey = "AIzaSyAVJFd7htTKbeo7if-p-NxCNOiVDdN7kdU";
+        
         String lat;
         String geoCodeURL = "https://maps.googleapis.com/maps/api/geocode/json";
-        String [] [] geoCodeParams = new String [] [] {{"key", "AIzaSyAVJFd7htTKbeo7if-p-NxCNOiVDdN7kdU"}, {"address", "27370"}};
-
-        String geoLocationURL = "https://www.googleapis.com/geolocation/v1/geolocate";
-        String [] [] geoLocationParams = new String [] [] {{"key", "AIzaSyAVJFd7htTKbeo7if-p-NxCNOiVDdN7kdU"}};
+        String [] [] geoCodeParams = new String [] [] {{"key", userKey}, {"address", "27409"}};
 
         APIModel geoCodeModel = new APIModel ();
-        APIController geoCodeControl = new APIController (geoCodeModel, geoCodeURL, geoCodeParams);
+        APIController geoCodeControl = new APIController (geoCodeModel, geoCodeURL, userKey, geoCodeParams);
 
         //get API return string.
         lat = geoCodeControl.getAPIString("GET", "results;geometry;location;lat");
         System.out.println (lat);
+        
+        String geoLocationURL = "https://www.googleapis.com/geolocation/v1/geolocate";
+        String [] [] geoLocationParams = new String [] [] {{"key", userKey}};
 
         //__________________________________
 
