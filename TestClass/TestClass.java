7402cbd9db01db04b12bd3ef93e6cc0cf9afcb41
darodriguez2 <darodriguez2@outlook.com>
2020-05-03 19:30:07 -0400

    minor changes to test

--- TestClass/TestClass.java
+++ TestClass/TestClass.java
@@ -1,8 +1,14 @@
 package TestClass;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * The method being tested is used in the PreparedStatement utility class and is called "checkForValidAttribute".
+ * The purpose of "checkForValidAttribute" is to to check for the validity of attribute types for attributes to be  
+ * inserted into the database. The attribute type should match one of the columns in the table "users". 
+ * 
+ * We can test this method by passing different strings to it and seeing if it returns true or false, depending on 
+ * the validity of the string. 
+ * 
+ * @author Diego Rodriguez
+ * @date 5/2/20
  */
 import utilities.DBConnectionUtil.PreparedStatementUtil;
 
@@ -15,8 +21,14 @@ PreparedStatementUtil testView = new PreparedStatementUtil();
     public static void main(String[] args) {
         TestClass test = new TestClass();
         PreparedStatementUtil testView = new PreparedStatementUtil();
+        
+        //error case
         System.out.println(test.testMethod("uuids"));
+        
+        //edge case, we are expecting it to return true even though the attribute is in all caps.
         System.out.println(test.testMethod("UUID"));
+        
+        //normal cases
         System.out.println(testView.checkForValidAttribute("uuid"));
         System.out.println(testView.checkForValidAttribute("firstname"));
         
